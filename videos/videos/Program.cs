909fd2c1c53cf01c44db0101dfb32a3f8211c5c4
Gerson <grecinosl3@miumg.edu.gt>
2026-02-19 14:06:11 -0600

    Primer commit agregando ejercicio 1-10

---
+++ videos/videos/Program.cs
@@ -0,0 +1,112 @@
+﻿
+//-----Imprimir en consola-----
+//Console.WriteLine(45);
+//Console.WriteLine("Informaticonfig");
+//Console.WriteLine(50 + 60);
+/*
+//-----Tipos de datos-----
+int = 1 25 355
+double = 3.14 4,566
+bool = true / false
+char = 'a';
+float = 1.50f
+string = 'asdasadsad'
+Object, class, interface
+*/
+
+/*
+//-----Variables-----
+string minombre = "informaticonfig";
+int edad = 20;
+bool ACTIVO = false;
+DateTime fecha = DateTime.Now;
+float precio = 1.500f;
+decimal descuento = 10.50m;
+double descuento2 = 10.50;
+Console.WriteLine(minombre);
+Console.WriteLine(edad);
+edad = 50;
+Console.WriteLine(edad);
+Console.WriteLine(fecha);
+Console.WriteLine(precio);
+Console.WriteLine(descuento);
+Console.WriteLine(descuento2);
+
+Console.ReadKey();
+*/
+
+/*
+//-----Uso de la concatenación-----
+string nombre = "Ana";
+int edad = 30;
+
+Console.WriteLine("Nombre de usuario: " + nombre +"\n" +
+                    "Edad: " + edad);
+
+Console.WriteLine($"Nombre usuario: {nombre}, edad: {edad}");
+Console.ReadKey();
+
+
+*/
+
+/*
+//-----CONSTANTES-----
+var numero = 10.20;
+Console.WriteLine(numero);
+numero = 3000;
+Console.WriteLine(numero);
+
+const int numero2 = 150;
+Console.WriteLine("Valor de constante: " + numero2);
+
+const double PI = 3.1416;
+const string mensaje = "Bienvenidos!";
+Console.ReadKey();
+*/
+
+/*
+//-----Casting-----
+//Implicito
+int valor = 200;
+double total = valor;
+Console.WriteLine("Implicito: " + total);
+
+//Explicito 
+double precio = 500.23;
+int descuento = (int)precio;
+Console.WriteLine("Explicito: " + descuento);
+
+char letra = 'A';
+int codigoAsci = letra;
+Console.WriteLine(codigoAsci);
+
+string palabra = "123456";
+int numero = Convert.ToInt32(palabra);
+Console.WriteLine(numero);
+
+Console.ReadKey();
+*/
+
+/*
+//-----Operadores Aritmeticos-----
+
+int valor1 = 90, valor2 = 70;
+var valor3 = 10;
+
+//Suma
+int total = valor1 + valor2;
+Console.WriteLine($"Total de la suma: {total}");
+//Resta
+int diferencia = valor1 - valor2;
+Console.WriteLine($"Total de la resta: {diferencia}");
+//Multiplicacion 
+var producto = valor3 * valor2;
+Console.WriteLine($"Total de la multiplicación: {producto}");
+//Division
+var resultado = valor1 / valor3;
+Console.WriteLine($"El resultado es: {resultado}");
+//Residuo
+var modulo = valor2 % valor3;
+Console.WriteLine($"El residuo es: {modulo}");
+Console.ReadKey();
+*/
